1e59625641a2138097f61916967812b01c428590
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2024-06-22 10:42:23 +0300

    done greeting

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -3,11 +3,11 @@ import {PagesWrapper} from '../pages-wrapper/pages-wrapper';
 import {useAPIGreeting} from '@/api/user/user';
 
 export const HomePage: FC = () => {
-  const greetingMutate = useAPIGreeting();
+  const {data} = useAPIGreeting();
   return (
     <PagesWrapper>
       <div className="bg-white w-[486px] rounded-r-2xl items-center justify-center flex flex-col px-[64px]">
-        <h2>ghbdsn</h2>
+        <h2>{data && data.data.message}</h2>
       </div>
     </PagesWrapper>
   );
