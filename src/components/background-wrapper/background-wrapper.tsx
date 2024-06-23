de7567a4ac7a95b16b21893c3d7db07c0611e100
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2024-06-23 13:41:56 +0300

    change title and add version

--- src/components/background-wrapper/background-wrapper.tsx
+++ src/components/background-wrapper/background-wrapper.tsx
@@ -1,12 +1,14 @@
 import {FC, ReactNode} from 'react';
 
+import pj from '../../../package.json';
 interface BackgroundWrapperProps {
   children: ReactNode;
 }
 
 export const BackgroundWrapper: FC<BackgroundWrapperProps> = ({children}) => {
   return (
-    <div className="bg-[#5769D4] w-screen h-screen items-center justify-center flex">
+    <div className="bg-[#5769D4] w-screen h-screen items-center justify-center flex relative">
+      <p className="absolute text-white top-2 left-2">v{pj.version}</p>
       {children}
     </div>
   );
