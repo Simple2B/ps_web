78353600d251b790984849d9b482a83196bfe85c
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2024-06-24 09:03:00 +0300

    remove refetch

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -3,11 +3,7 @@ import {PagesWrapper} from '../pages-wrapper/pages-wrapper';
 import {useAPIGreeting} from '@/api/user/user';
 
 export const HomePage: FC = () => {
-  const {data} = useAPIGreeting({
-    query: {
-      enabled: false,
-    },
-  });
+  const {data} = useAPIGreeting();
   return (
     <PagesWrapper>
       <div className="bg-white w-[486px] rounded-r-2xl items-center justify-center flex flex-col px-[64px]">
